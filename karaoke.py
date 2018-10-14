502b890386898550e4a89533888409dd9ee1b21b
Marcelo Cubria <marcelocubria@gmail.com>
2018-10-14 16:56:32 +0200

    cambia valores link a local (no se ve al imprimir, lo cambia despues)

--- karaoke.py
+++ karaoke.py
@@ -15,15 +15,22 @@ if __name__ == "__main__":
     except FileNotFoundError:
         print("Usage: python3 karaoke.py file.smil")
     parser.parse(fichero)
-    for index, etiqueta in enumerate(sHandler.tag_list):
-        if etiqueta in sHandler.tags and sHandler.tag_list[index+1] == sHandler.tags[etiqueta][0]:
-            linea = sHandler.tag_list[index] + "\t"
+    lista = sHandler.get_tags()
+    for index, etiqueta in enumerate(lista):
+        # comprueba que si src no es etiqueta si no atributo no lo cuente
+        if etiqueta in sHandler.tags and lista[index+1] == sHandler.tags[etiqueta][0]:
+            linea = lista[index] + "\\t"
             for index2, atributo in enumerate(sHandler.tags[etiqueta]):
-                linea += sHandler.tag_list[index+1+index2*2] + "="
-                linea += sHandler.tag_list[index+2+index2*2] + "\t"
+                linea += lista[index+1+index2*2] + "="
+                linea += lista[index+2+index2*2] + "\\t"
+            linea = linea[:-2]
+            linea += "\\n"
             print(linea)
+        # este if descarga archivos remotos y cambia a local
         if etiqueta == "src":
-            link = sHandler.tag_list[index + 1]
+            link = lista[index + 1]
             if link[0:7] == "http://":
                 nombre_archivo = "archivo" + str(index)
                 urllib.request.urlretrieve(link, nombre_archivo)
+                link = link.split("/")[-1]
+                lista[index + 1] = link
