3e1f4dd2752b3c78c8b9a5de07e57d292cbb5546
Marcelo Cubria <marcelocubria@gmail.com>
2018-10-14 16:13:06 +0200

    añadido apartado 5

--- karaoke.py
+++ karaoke.py
@@ -4,16 +4,17 @@
 from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
 import sys
+import urllib.request
 
 if __name__ == "__main__":
     parser = make_parser()
     sHandler = SmallSMILHandler()
     parser.setContentHandler(sHandler)
     try:
-        parser.parse(open(sys.argv[1]))
+        fichero = open(sys.argv[1])
     except FileNotFoundError:
         print("Usage: python3 karaoke.py file.smil")
-    print(len(sHandler.tag_list))
+    parser.parse(fichero)
     for index, etiqueta in enumerate(sHandler.tag_list):
         if etiqueta in sHandler.tags and sHandler.tag_list[index+1] == sHandler.tags[etiqueta][0]:
             linea = sHandler.tag_list[index] + "\t"
@@ -21,3 +22,8 @@ if __name__ == "__main__":
                 linea += sHandler.tag_list[index+1+index2*2] + "="
                 linea += sHandler.tag_list[index+2+index2*2] + "\t"
             print(linea)
+        if etiqueta == "src":
+            link = sHandler.tag_list[index + 1]
+            if link[0:7] == "http://":
+                nombre_archivo = "archivo" + str(index)
+                urllib.request.urlretrieve(link, nombre_archivo)
